1c95c642c20f87f892a1bd74a9e8944b42f98882
Clem/mikazia <122747853+mikazia@users.noreply.github.com>
2023-04-02 05:16:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+🌷 Flowamath 🌷
+--------
+
+flowamath est un module node.js conçu pour les expressions mathématiques :)
+
+🌷✅🌷✌️🌷✌️🌷✌️🌷✌️🌷✌️🌷✌️✌️🌷
+
+exemple de programme avec flowamath
+
+```
+const InitMath  = require('flowamath');
+const math = new InitMath();
+// Define coefficients
+let a = 2;
+let b = 4;
+let c = 6;
+let solution = math.linearFunction(a, b, c);
+
+// Output the solution
+console.log(`The solution of the equation ${a}x + ${b} = ${c} is ${solution}.`);
+``
+🌷🌸🌷🌸🌷🌸🌷💕🌷🌷🌸🌷🌸🌸🌷🌸🌷🌸🌷
+pour plus d'exemple voir le wiki du repos flowamath.js de mikazia sur github ❣️❣️❣️❣️❣️❣️❣️❣️♥️♥️♥️
---
+++ index.js
@@ -0,0 +1,29 @@
+class InitMath {
+  constructor() {
+    this.math = require('mathjs');
+  }
+
+  // Initializes a math client
+  init() {
+    return this.math;
+  }
+linearFunction(a, b, c) {
+    // Check if valid input
+    if (a === 0) {
+        return "Invalide ❌ merci dentrer une valeur d'équation valide.";
+    }
+    
+    // Calculate the solution
+    let solution = (c - b) / a;
+    
+    // Print the solution to the console
+    console.log(`The solution of the equation is ${solution}.`);
+    
+    // Return the solution
+    return solution;
+}
+add(a, b) {
+    return a + b;
+  }
+}
+module.exports = InitMath;
